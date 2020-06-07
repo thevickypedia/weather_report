636688f6ab37a3abe7aa6a05dc676b502b065d48
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-06-07 14:39:20 -0500

    dedicated functions to improve coding standards

--- weather.py
+++ weather.py
@@ -1,36 +1,28 @@
-import urllib.request
 import json
-import pytemperature
 import os
 import ssl
+import urllib.request
+from datetime import datetime
+
 import certifi
 import geopy.geocoders
+import pytemperature
 from geopy.geocoders import Nominatim
 
-api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
-
-# city = input('Enter city:\n')
+# cities = input('Enter city:\n')
 city = 'Springfield'
-api_key = os.getenv('api_key')
 
-url = api_endpoint + "?q=" + city + "&appid=" + api_key
-response = urllib.request.urlopen(url)
 
-parseResponse = json.loads(response.read())
+def parseResponse():
+    api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
+    api_key = os.getenv('api_key')
+    url = api_endpoint + "?q=" + city + "&appid=" + api_key
+    response = urllib.request.urlopen(url)
+    parse_response = json.loads(response.read())
+    return parse_response
 
-country = parseResponse['sys']['country']
-temperature = parseResponse['main']['temp']
-weather = parseResponse['weather'][0]['description']
-feels_like = parseResponse['main']['feels_like']
-temp_mn = parseResponse['main']['temp_min']
-temp_mx = parseResponse['main']['temp_max']
 
-temp_f = float(round(pytemperature.k2f(temperature), 2))
-temp_c = float(round(pytemperature.k2c(temperature), 2))
-temp_feel_f = float(round(pytemperature.k2f(feels_like), 2))
-temp_feel_c = float(round(pytemperature.k2c(feels_like), 2))
-temp_min = float(round(pytemperature.k2c(temp_mn), 2))
-temp_max = float(round(pytemperature.k2c(temp_mx), 2))
+parseResponse = parseResponse()
 
 
 def location():
@@ -49,8 +41,30 @@ def location():
     return county
 
 
-output = (f'{city}, {location()}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
-          f'Feels Like: {temp_feel_f}°F\nMinimum: {temp_min}°F Maximum: {temp_max}°F\n'
-          f'Condition: {weather}')
+def result():
+    country = parseResponse['sys']['country']
+    temperature = parseResponse['main']['temp']
+    weather = parseResponse['weather'][0]['description']
+    feels_like = parseResponse['main']['feels_like']
+    temp_mn = parseResponse['main']['temp_min']
+    temp_mx = parseResponse['main']['temp_max']
 
-print(output)
+    temp_f = float(round(pytemperature.k2f(temperature), 2))
+    temp_c = float(round(pytemperature.k2c(temperature), 2))
+    temp_feel_f = float(round(pytemperature.k2f(feels_like), 2))
+    temp_feel_c = float(round(pytemperature.k2c(feels_like), 2))
+    temp_min = float(round(pytemperature.k2c(temp_mn), 2))
+    temp_max = float(round(pytemperature.k2c(temp_mx), 2))
+
+    output = (f'{city}, {location()}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
+              f'Feels Like: {temp_feel_f}°F\nMinimum: {temp_min}°F Maximum: {temp_max}°F\n'
+              f'Condition: {weather}')
+
+    return output
+
+
+if __name__ == '__main__':
+    now = datetime.now()
+    dt_string = now.strftime("%A, %B %d, %Y %I:%M %p")
+    print(dt_string)
+    print(result())
