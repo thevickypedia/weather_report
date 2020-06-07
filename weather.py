549d310a1568d63bb0908c2162bb9d5047873b26
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-06-07 14:28:34 -0500

    include location accuracy as the current version will only print city and country and leaves with no clue on which city of state

--- weather.py
+++ weather.py
@@ -2,6 +2,10 @@ import urllib.request
 import json
 import pytemperature
 import os
+import ssl
+import certifi
+import geopy.geocoders
+from geopy.geocoders import Nominatim
 
 api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
 
@@ -28,7 +32,24 @@ temp_feel_c = float(round(pytemperature.k2c(feels_like), 2))
 temp_min = float(round(pytemperature.k2c(temp_mn), 2))
 temp_max = float(round(pytemperature.k2c(temp_mx), 2))
 
-output = (f'{city}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
+
+def location():
+    # This is to ensure your request to geopy goes through a cert
+    ctx = ssl.create_default_context(cafile=certifi.where())
+    geopy.geocoders.options.default_ssl_context = ctx
+
+    coordinates = parseResponse['coord']
+    lon = (coordinates['lon'])
+    lat = (coordinates['lat'])
+    geo_locator = Nominatim(scheme='http', user_agent='test/1', timeout=3)
+    locator = geo_locator.reverse(f'{lat}, {lon}')
+    current_location = locator.address
+    c_l = current_location.split(', ')
+    county = f'{c_l[4]}, {c_l[5]}, {c_l[7]}'
+    return county
+
+
+output = (f'{city}, {location()}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
           f'Feels Like: {temp_feel_f}°F\nMinimum: {temp_min}°F Maximum: {temp_max}°F\n'
           f'Condition: {weather}')
 
