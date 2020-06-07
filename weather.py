008c6241fbb885d89df6b9b65be780214ace48cc
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-06-07 14:26:53 -0500

    finalized necessary info

--- weather.py
+++ weather.py
@@ -14,7 +14,22 @@ response = urllib.request.urlopen(url)
 
 parseResponse = json.loads(response.read())
 
+country = parseResponse['sys']['country']
 temperature = parseResponse['main']['temp']
 weather = parseResponse['weather'][0]['description']
+feels_like = parseResponse['main']['feels_like']
+temp_mn = parseResponse['main']['temp_min']
+temp_mx = parseResponse['main']['temp_max']
 
-print(float(round(pytemperature.k2f(temperature), 2)))
+temp_f = float(round(pytemperature.k2f(temperature), 2))
+temp_c = float(round(pytemperature.k2c(temperature), 2))
+temp_feel_f = float(round(pytemperature.k2f(feels_like), 2))
+temp_feel_c = float(round(pytemperature.k2c(feels_like), 2))
+temp_min = float(round(pytemperature.k2c(temp_mn), 2))
+temp_max = float(round(pytemperature.k2c(temp_mx), 2))
+
+output = (f'{city}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
+          f'Feels Like: {temp_feel_f}°F\nMinimum: {temp_min}°F Maximum: {temp_max}°F\n'
+          f'Condition: {weather}')
+
+print(output)
