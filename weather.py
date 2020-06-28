3d2e7c60f510148f813378978433322ef25bebaa
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-06-28 09:16:24 -0500

    file name change

--- weather.py
+++ weather.py
@@ -3,98 +3,71 @@ import os
 import ssl
 import urllib.parse
 import urllib.request
-from datetime import datetime
+from datetime import datetime, date
 
 import certifi
 import geopy.geocoders
 import pytemperature
-from bs4 import BeautifulSoup as bs
 from geopy.geocoders import Nominatim
 
-# city = input('Enter city:\n')
 city = 'Springfield'
+state = 'Missouri'
 
 
-def parseResponse():
-    """This function gets the raw weather information"""
-    api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
-    api_key = os.getenv('api_key')
-    url = api_endpoint + "?q=" + city + "&appid=" + api_key  # creates an url
-    try:
-        response = urllib.request.urlopen(url)  # sends request to the url created
-        parse_response = json.loads(response.read())  # loads the response in a json
-        return parse_response
-    except:
-        if len(city) < 1:
-            print('Please enter a city name to continue.')
-            exit()
-        else:
-            # passes headers to access browser
-            headers = {'User-Agent': "Mozilla/5.0 (X11; Linux i686)"}
-            # stores incorrect city value in the format that can combined to an url
-            values = {'q': f'{city}'}
-            # parses the stored value and generates a url code
-            data = urllib.parse.urlencode(values)
-            url = f'https://www.google.com/search?{data}'
-            req = urllib.request.Request(url, headers=headers)  # sends a request tot he url
-            resp = urllib.request.urlopen(req)
-            resp_data = resp.read()  # reads the response
-            soup = bs(resp_data, 'html.parser')  # html parsing
-            data = soup.find_all('div', {'class': 'Pg70bf Uv67qb'})  # looks for the right class under div
-            raw = data[0].find_all('a')  # looks for tag <a></a>
-            url_ = raw[0].get('href')  # gets the href url eg: https://maps.google.com/search?q:{suggestion}
-            suggested_city = (url_.split('=')[1].replace('&source', '').replace('&um', ''))  # extracts value from url
-            print(f"Trying '{suggested_city}' instead of '{city}'\n")
-            try:
-                api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
-                api_key = os.getenv('api_key')
-                url = api_endpoint + "?q=" + suggested_city + "&appid=" + api_key  # creates an url
-                response = urllib.request.urlopen(url)  # sends request to the url created
-                parse_response = json.loads(response.read())  # loads the response in a json
-                return parse_response
-            except:
-                print('Please try with a valid city name.')
-                exit()
-
-
-def location():
-    """This function gets the location information using reverse geocoding"""
-    # This is to ensure your request to geopy goes through a cert
+def today_parse():
     ctx = ssl.create_default_context(cafile=certifi.where())
     geopy.geocoders.options.default_ssl_context = ctx
-    # extracts latitude and longitude information
-    coordinates = parseResponse['coord']
-    lon = (coordinates['lon'])
-    lat = (coordinates['lat'])
-    # gets location information using the latitude and longitude values
     geo_locator = Nominatim(scheme='http', user_agent='test/1', timeout=3)
-    locator = geo_locator.reverse(f'{lat}, {lon}')
-    current_location = locator.address  # extracts the address part
-    c_l = current_location.split(', ')
     try:
-        county = f'{c_l[-5]}, {c_l[-4]}, {c_l[-3]}, {c_l[-1]}'  # gets only the county, state and/or country information
-    except IndexError:
-        county = current_location  # if raised index error stores the entire address here
-    return county
+        location = geo_locator.geocode(f'{city}, {state}')
+        lat = location.latitude
+        lon = location.longitude
+        city_info = location.address
+        api_endpoint = "http://api.openweathermap.org/data/2.5/"
+        api_key = os.getenv('api_key')
+        url = f'{api_endpoint}onecall?lat={lat}&lon={lon}&exclude=daily,minutely&appid={api_key}'
+        r = urllib.request.urlopen(url)  # sends request to the url created
+        parse_response = json.loads(r.read())  # loads the response in a json
+
+        return parse_response, city_info
+    except AttributeError:
+        print('Make sure the city and state combination is correct.\nIf you are unsure of either of it you may leave '
+              'it blank rather incorrect.')
+        exit(1)
+    except NameError:
+        print('Make sure either city or state is entered.')
+        exit(1)
 
 
-def result():
-    """This function is to get detailed weather information"""
-    country = parseResponse['sys']['country']
-    temperature = parseResponse['main']['temp']
-    weather = parseResponse['weather'][0]['description']
-    feels_like = parseResponse['main']['feels_like']
-    temp_mn = parseResponse['main']['temp_min']
-    temp_mx = parseResponse['main']['temp_max']
+def today():
+    import time
+    hourly_data = (response['hourly'])
+    output = ''
+    for data in hourly_data:
+        epoch = data['dt']
+        timestamp = time.strftime('%m-%d %H Hrs', time.localtime(epoch))
+        today = date.today().strftime('%m-%d')
+        if today in timestamp:
+            temperature = data['temp']
+            weather = data['weather'][0]['description']
+            feels_like = data['feels_like']
+            temp_f = float(round(pytemperature.k2f(temperature), 2))
+            temp_feel_f = float(round(pytemperature.k2f(feels_like), 2))
+            output += (f'{timestamp}: Actual: {temp_f}°F '
+                       f'Feels Like: {temp_feel_f}°F Condition: {weather}\n')
+    output_ = f'{Weather_location}\n{output}'
 
+    return output_
+
+
+def current_result():
+    temperature = response['current']['temp']
+    weather = response['current']['weather'][0]['description']
+    feels_like = response['current']['feels_like']
     temp_f = float(round(pytemperature.k2f(temperature), 2))
     temp_feel_f = float(round(pytemperature.k2f(feels_like), 2))
-    temp_min = float(round(pytemperature.k2f(temp_mn), 2))
-    temp_max = float(round(pytemperature.k2f(temp_mx), 2))
-
-    output = (f'{location}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
-              f'Feels Like: {temp_feel_f}°F\nHigh: {temp_max}°F Low: {temp_min}°F\n'
-              f'Condition: {weather}')
+    output = (f'{Weather_location}\nCurrent Temperature: {temp_f}°F\n'
+              f'Feels Like: {temp_feel_f}°F\nCondition: {weather}')
 
     return output
 
@@ -102,7 +75,9 @@ def result():
 if __name__ == '__main__':
     now = datetime.now()
     dt_string = now.strftime("%A, %B %d, %Y %I:%M %p")
-    parseResponse = parseResponse()
-    location = location()
+    response, Weather_location = today_parse()
     print(f'{dt_string}\n')
-    print(result())
+    print(current_result())
+    user_input = input('\nPress y to view the detailed weather report for today:\n')
+    if 'Y' or 'y' in user_input:
+        print(f"Today's Report with per hour interval:\n{today()}")
---
+++ weather_old.py
@@ -0,0 +1,108 @@
+import json
+import os
+import ssl
+import urllib.parse
+import urllib.request
+from datetime import datetime
+
+import certifi
+import geopy.geocoders
+import pytemperature
+from bs4 import BeautifulSoup as bs
+from geopy.geocoders import Nominatim
+
+# city = input('Enter city:\n')
+city = 'Springfield'
+
+
+def parseResponse():
+    """This function gets the raw weather information"""
+    api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
+    api_key = os.getenv('api_key')
+    url = api_endpoint + "?q=" + city + "&appid=" + api_key  # creates an url
+    try:
+        response = urllib.request.urlopen(url)  # sends request to the url created
+        parse_response = json.loads(response.read())  # loads the response in a json
+        return parse_response
+    except:
+        if len(city) < 1:
+            print('Please enter a city name to continue.')
+            exit()
+        else:
+            # passes headers to access browser
+            headers = {'User-Agent': "Mozilla/5.0 (X11; Linux i686)"}
+            # stores incorrect city value in the format that can combined to an url
+            values = {'q': f'{city}'}
+            # parses the stored value and generates a url code
+            data = urllib.parse.urlencode(values)
+            url = f'https://www.google.com/search?{data}'
+            req = urllib.request.Request(url, headers=headers)  # sends a request tot he url
+            resp = urllib.request.urlopen(req)
+            resp_data = resp.read()  # reads the response
+            soup = bs(resp_data, 'html.parser')  # html parsing
+            data = soup.find_all('div', {'class': 'Pg70bf Uv67qb'})  # looks for the right class under div
+            raw = data[0].find_all('a')  # looks for tag <a></a>
+            url_ = raw[0].get('href')  # gets the href url eg: https://maps.google.com/search?q:{suggestion}
+            suggested_city = (url_.split('=')[1].replace('&source', '').replace('&um', ''))  # extracts value from url
+            print(f"Trying '{suggested_city}' instead of '{city}'\n")
+            try:
+                api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
+                api_key = os.getenv('api_key')
+                url = api_endpoint + "?q=" + suggested_city + "&appid=" + api_key  # creates an url
+                response = urllib.request.urlopen(url)  # sends request to the url created
+                parse_response = json.loads(response.read())  # loads the response in a json
+                return parse_response
+            except:
+                print('Please try with a valid city name.')
+                exit()
+
+
+def location():
+    """This function gets the location information using reverse geocoding"""
+    # This is to ensure your request to geopy goes through a cert
+    ctx = ssl.create_default_context(cafile=certifi.where())
+    geopy.geocoders.options.default_ssl_context = ctx
+    # extracts latitude and longitude information
+    coordinates = parseResponse['coord']
+    lon = (coordinates['lon'])
+    lat = (coordinates['lat'])
+    # gets location information using the latitude and longitude values
+    geo_locator = Nominatim(scheme='http', user_agent='test/1', timeout=3)
+    locator = geo_locator.reverse(f'{lat}, {lon}')
+    current_location = locator.address  # extracts the address part
+    c_l = current_location.split(', ')
+    try:
+        county = f'{c_l[-5]}, {c_l[-4]}, {c_l[-3]}, {c_l[-1]}'  # gets only the county, state and/or country information
+    except IndexError:
+        county = current_location  # if raised index error stores the entire address here
+    return county
+
+
+def result():
+    """This function is to get detailed weather information"""
+    country = parseResponse['sys']['country']
+    temperature = parseResponse['main']['temp']
+    weather = parseResponse['weather'][0]['description']
+    feels_like = parseResponse['main']['feels_like']
+    temp_mn = parseResponse['main']['temp_min']
+    temp_mx = parseResponse['main']['temp_max']
+
+    temp_f = float(round(pytemperature.k2f(temperature), 2))
+    temp_feel_f = float(round(pytemperature.k2f(feels_like), 2))
+    temp_min = float(round(pytemperature.k2f(temp_mn), 2))
+    temp_max = float(round(pytemperature.k2f(temp_mx), 2))
+
+    output = (f'{location}, {country}\n\nCurrent Temperature: {temp_f}°F\n'
+              f'Feels Like: {temp_feel_f}°F\nHigh: {temp_max}°F Low: {temp_min}°F\n'
+              f'Condition: {weather}')
+
+    return output
+
+
+if __name__ == '__main__':
+    now = datetime.now()
+    dt_string = now.strftime("%A, %B %d, %Y %I:%M %p")
+    parseResponse = parseResponse()
+    location = location()
+    print(f'{dt_string}\n')
+    print(result())
