da902eb27bd3610e12e4edfd17c787fe50fc9b00
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-15 16:18:19 -0500

    get location using IP

--- test.py
+++ test.py
@@ -3,11 +3,18 @@ from urllib.request import urlopen
 import json
 
 
+print('Your IP details:\n ')
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
+print(f'Origin IP: {s.getsockname()[0]}')
 
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.load(response)
-print(data)
+ip = data['ip']
+org = data['org']
+city = data['city']
+country = data['country']
+region = data['region']
+
+print(f'Destination IP: {ip}\nCity: {city}\nState: {region}\nCountry: {country}\nISP: {org}')
