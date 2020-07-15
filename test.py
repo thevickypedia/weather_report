763747249d5f4113941e953d20d110b5cce6d06b
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-15 16:13:33 -0500

    get ip data

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import socket
+import urllib.request
+import json
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 print(s.getsockname()[0])
+
+response = urllib.request.urlopen("https://geolocation-db.com/json")
+data = json.loads(response.read().decode())
+print(data)
