3bc96772db675d1d346e8b1d21f7ae95ca1c8da1
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-07-16 13:46:38 -0500

    remove reverse geocoding

--- weather_by_location.py
+++ weather_by_location.py
@@ -1,49 +1,29 @@
 import json
 import os
-import ssl
-import urllib.parse
-import urllib.request
+from urllib.request import urlopen
 from datetime import datetime, date
 
-import certifi
-import geopy.geocoders
 import pytemperature
-from geopy.geocoders import Nominatim
 
 
 def get_location():
     url = 'http://ipinfo.io/json'
-    resp = urllib.request.urlopen(url)
+    resp = urlopen(url)
     data = json.load(resp)
-    city = data['city']
-    state = data['region']
 
-    return city, state
+    return data['city'], data['region'], data['country'], data['loc']
 
 
 def today_parse():
-    ctx = ssl.create_default_context(cafile=certifi.where())
-    geopy.geocoders.options.default_ssl_context = ctx
-    geo_locator = Nominatim(scheme='http', user_agent='test/1', timeout=3)
-    try:
-        location = geo_locator.geocode(f'{get_location()}')
-        lat = location.latitude
-        lon = location.longitude
-        city_info = location.address
-        api_endpoint = "http://api.openweathermap.org/data/2.5/"
-        api_key = os.getenv('api_key')
-        url = f'{api_endpoint}onecall?lat={lat}&lon={lon}&exclude=daily,minutely&appid={api_key}'
-        r = urllib.request.urlopen(url)  # sends request to the url created
-        parse_response = json.loads(r.read())  # loads the response in a json
+    lat = coordinates.split(',')[0]
+    lon = coordinates.split(',')[1]
+    api_endpoint = "http://api.openweathermap.org/data/2.5/"
+    api_key = os.getenv('api_key')
+    url = f'{api_endpoint}onecall?lat={lat}&lon={lon}&exclude=daily,minutely&appid={api_key}'
+    r = urlopen(url)  # sends request to the url created
+    parse_response = json.loads(r.read())  # loads the response in a json
 
-        return parse_response, city_info
-    except AttributeError:
-        print('Make sure the city and state combination is correct.\nIf you are unsure of either of it you may leave '
-              'it blank rather incorrect.')
-        exit(1)
-    except NameError:
-        print('Make sure either city or state is entered.')
-        exit(1)
+    return parse_response
 
 
 def today():
@@ -82,9 +62,11 @@ def current_result():
 if __name__ == '__main__':
     now = datetime.now()
     dt_string = now.strftime("%A, %B %d, %Y %I:%M %p")
-    response, Weather_location = today_parse()
+    city, state, country, coordinates = get_location()
+    Weather_location = f'{city}, {state}, {country}'
+    response = today_parse()
     print(f'{dt_string}\n')
     print(current_result())
     # user_input = input('\nPress y to view the detailed weather report for today:\n')
-    # if 'Y' or 'y' in user_input:
+    # if user_input == 'Y' or user_input == 'y':
     print(f"\nToday's Report with per hour interval:\n{today()}")
